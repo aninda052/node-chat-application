8d7d67a5c35e11ebb83c65e3c6f1b882a7f5ad87
aninda052 <anindahalder052@gmail.com>
2022-12-21 01:20:12 +0600

    separate error response for html and REST Api

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -7,9 +7,18 @@ function notFoundHandler(req, res, next) {
 
 // default error handler
 function errorHandler(err, req, res, next) {
-  res.render("error", {
-    title: "Error page",
-  });
+  res.locals.error =
+    process.env.NODE_ENV === "development" ? err : { message: err.message };
+
+  res.status(err.status || 500);
+
+  if (!res.locals.http) {
+    res.render("error", {
+      title: "Error page",
+    });
+  } else {
+    res.json(res.locals.error);
+  }
 }
 
 module.exports = {
